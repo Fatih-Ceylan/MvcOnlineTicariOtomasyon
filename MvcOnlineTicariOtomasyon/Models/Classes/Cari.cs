459d961f1ddba3e64b7cbe3f59f74d218ee61ed0
Fatih Ceylan <fceylan001@gmail.com>
2024-10-18 22:20:48 +0300

    .

---
+++ MvcOnlineTicariOtomasyon/Models/Classes/Cari.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcOnlineTicariOtomasyon.Models.Classes
+{
+    public class Cari
+    {
+        [Key]
+        public int CariId { get; set; }
+        public string CariAd { get; set; }
+        public string CariSoyad { get; set; }
+        public string CariSehir { get; set; }
+        public string CariMail { get; set; }
+    }
+}
--- MvcOnlineTicariOtomasyon/Models/Classes/Personel.cs
+++ MvcOnlineTicariOtomasyon/Models/Classes/Personel.cs
@@ -1,9 +1,13 @@
-﻿namespace MvcOnlineTicariOtomasyon.Models.Classes
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MvcOnlineTicariOtomasyon.Models.Classes
 {
     public class Personel
     {
+        [Key]
         public int PersonelId { get; set; }
         public string PersonelAd { get; set; }
         public string PersonelSoyad { get; set; }
+        public string PersonelGorsel{ get; set; }
     }
 }
