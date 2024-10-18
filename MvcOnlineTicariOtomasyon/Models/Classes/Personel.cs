fa98f2b8220bb7e889272ced3dbc998a24101e4e
Fatih Ceylan <fceylan001@gmail.com>
2024-10-18 22:19:34 +0300

    .

---
+++ MvcOnlineTicariOtomasyon/Models/Classes/Personel.cs
@@ -0,0 +1,9 @@
+﻿namespace MvcOnlineTicariOtomasyon.Models.Classes
+{
+    public class Personel
+    {
+        public int PersonelId { get; set; }
+        public string PersonelAd { get; set; }
+        public string PersonelSoyad { get; set; }
+    }
+}
