df385118f2bbb4b7a6e74b8a9bf4b75cff112c31
Fatih Ceylan <fceylan001@gmail.com>
2024-11-02 16:29:58 +0300

    faturaController eklendi

---
+++ MvcOnlineTicariOtomasyon/Controllers/FaturaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcOnlineTicariOtomasyon.Controllers
+{
+    public class FaturaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
