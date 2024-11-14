f19149ed15289ac897a3a9c9602c7bd6fb90de6c
Fatih Ceylan <fceylan001@gmail.com>
2024-11-14 15:15:29 +0300

    cari için login sayfası ve yeni tema eklendi

--- MvcOnlineTicariOtomasyon/Models/Classes/Personel.cs
+++ MvcOnlineTicariOtomasyon/Models/Classes/Personel.cs
@@ -1,4 +1,5 @@
 ﻿using MvcOnlineTicariOtomasyon.Models.Classes.Hareketler;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -11,14 +12,17 @@ namespace MvcOnlineTicariOtomasyon.Models.Classes
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [DisplayName("Personel Adı")]
         public string? PersonelAd { get; set; }
 
         [Column(TypeName = "Varchar")]
         [StringLength(30)]
+        [DisplayName("Personel Soyadı")]
+
         public string? PersonelSoyad { get; set; }
         [Column(TypeName = "Varchar")]
         [StringLength(250)]
-
+        [DisplayName("Personel Görsel")]
         public string? PersonelGorsel { get; set; }
 
         public virtual ICollection<SatisHareket>? SatisHareketleri { get; set; }
