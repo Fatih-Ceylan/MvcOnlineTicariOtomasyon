0b63050107fed1bcab8fa1a6466ed277c980292c
Fatih Ceylan <fceylan001@gmail.com>
2024-11-29 17:08:53 +0300

    kargoDetay added

---
+++ MvcOnlineTicariOtomasyon/Models/Classes/Kargo/KargoDetay.cs
@@ -0,0 +1,12 @@
+﻿namespace MvcOnlineTicariOtomasyon.Models.Classes.Kargo
+{
+    public class KargoDetay
+    {
+        public int KargoDetayId { get; set; }
+        public string? Aciklama { get; set; }
+        public string? TakipKodu { get; set; }
+        public string? Personel { get; set; }
+        public string? Alici { get; set; }
+        public DateTime Tarih { get; set; }
+    }
+}
