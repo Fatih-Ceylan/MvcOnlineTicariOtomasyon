746a4819060b1fc99b8d6a75e893d11f1ba719d4
Fatih Ceylan <fceylan001@gmail.com>
2024-12-01 16:22:11 +0300

    kargo takip sayfası güncellendi

--- MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
+++ MvcOnlineTicariOtomasyon/Controllers/KargoController.cs
@@ -8,19 +8,36 @@ namespace MvcOnlineTicariOtomasyon.Controllers
     public class KargoController(OtomasyonContext c) : Controller
     {
        
-        public IActionResult Index()
+        public IActionResult Index(string p)
         {
-            var kargolar = c.KargoDetay.ToList();
+            var k = c.KargoDetay.ToList();
+            if (!string.IsNullOrEmpty(p))
+            {
+                var kargolar = k
+                            .Where(x => x.TakipKodu != null &&
+                             x.TakipKodu.Contains(p, StringComparison.OrdinalIgnoreCase))
+                            .ToList();
 
-            return View(kargolar);
+                return View(kargolar);
+            }
+            return View(k);
         }
 
         [HttpGet]
         public IActionResult YeniKargo()
         {
-            Random rnd = new Random();  
+            var chars = "ABCDEFG0123456789";
+            var stringChars = new char[10];
+            var random = new Random();
 
+            for (int i = 0; i < stringChars.Length; i++)
+            {
+                stringChars[i] = chars[random.Next(chars.Length)];
+            }
 
+            var finalString = new String(stringChars);
+
+            ViewBag.TakipNo = finalString;
             return View();
         }
 
@@ -31,6 +48,13 @@ namespace MvcOnlineTicariOtomasyon.Controllers
             c.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        public IActionResult KargoDetay(int id)
+        {
+            var detay = c.KargoTakip.Where(x => x.KargoTakipId == id).ToList();
+             
+            return View(detay);
         }
     }
 }
